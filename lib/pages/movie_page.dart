56a7e098b0b3f2d21f810fee13544d3af14a1f7f
PrantoMondal <prantomondal517@gmail.com>
2023-01-24 12:41:06 +0600

    Movie model class added

---
+++ lib/pages/movie_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MoviePage extends StatefulWidget {
+  const MoviePage({Key? key}) : super(key: key);
+
+  @override
+  State<MoviePage> createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movie List'),
+      ),
+    );
+  }
+}
